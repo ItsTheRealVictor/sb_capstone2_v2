cdca1055d9246705d73a8305f80e67a83a4d9715
Victor Delgado <valexdelgado@gmail.com>
2023-05-20 14:28:08 -0700

    confusion

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -2,34 +2,27 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Collection from '../components/Collection';
+import Typography from '@mui/material/Typography';
+import Note from '../components/Note'
+
+const RandomBoxGenerator = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = () => {
+    const newNote = <Note/>
 
 
-const CollectionGenerator = () => {
-// Inside the CollectionGenerator component
-const [collection, setCollection] = useState([]);
+    setNotes([...notes, newNote]);
+  };
 
-const addCollection = () => {
-  const newCollection = <Collection notes={[]} />;
-  setCollection([...collection, newCollection]);
+  return (
+    <div>
+      <Button variant="contained" onClick={addNote}>
+        Add Note
+      </Button>
+      <div style={{ marginTop: '10px' }}>{notes}</div>
+    </div>
+  );
 };
 
-// ...
-
-return (
-  <div>
-    {collection.map((item, index) => (
-      <Card key={index}>
-        <CardContent>
-          {item}
-        </CardContent>
-      </Card>
-    ))}
-    <Button variant="contained" onClick={addCollection}>
-      Add Collection
-    </Button>
-  </div>
-);
-
-};
-
-export default CollectionGenerator;
+export default RandomBoxGenerator;
